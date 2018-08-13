b2ae0b7cc6d2689f15149704d589fc07202a1023
Jon Culver <jculver@cisco.com>
2018-08-13 22:24:43 +0100

    Fix typo

--- ffparser.py
+++ ffparser.py
@@ -91,7 +91,7 @@ class FFParser(object):
                                        reason     = player.reason,
                                        name       = player.name,
                                        last_season = player.last_season)
-            if db_entry.status = None:
+            if db_entry.status == None:
                 db_entry.status = ffdb.FFDBPlayer.FIT
             if player.total:
                 db_entry.set_total_score(self.week, player.total)
